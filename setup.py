a7669700954e8986c4274c68540abec506f7589c
Cian Synnott <cian@emauton.org>
2019-08-09 14:59:44 +0100

    Bump Flask version & add .gitignore

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ except ImportError:
 requirements = [
     'prometheus_client==0.5.0',
     'uwsgi==2.0.18',
-    'Flask==0.10.1',
+    'Flask==1.1.1',
 ]
 
 setup(
     name='pandoras_flask',
-    version='1.0.0',
+    version='1.0.1',
     description='Flask app with Prometheus monitoring',
     long_description='Worked example of integrating Prometheus monitoring with a Flask app',
     url='https://github.com/metricfire/pandoras_flask',
